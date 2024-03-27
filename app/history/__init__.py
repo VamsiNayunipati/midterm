ad362c936a337762274a5f08a277a7dfaceeb0df
VamsiNayunipati <vamsichowdary436@gmail.com>
2024-03-26 20:00:03 -0400

    adding calculator path for fetching history

---
+++ app/history/__init__.py
@@ -0,0 +1,33 @@
+import os
+import logging
+import pandas as pd
+
+class History:
+    def __init__(self):
+        path= os.environ.get('CALCULATOR_PATH')
+        self.path = path
+        self.counter = 1
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+            logging.info(f"The directory '{os.path.dirname(path)}' is created.")
+
+        elif not os.access(os.path.dirname(path), os.W_OK):
+            logging.error(f"The directory '{os.path.dirname(path)}' is not writable.")
+
+    def data_input(self, data):
+        data_without_id = [record[1:] if len(record) == 4 else record for record in data]
+        data_with_id = [[self.counter + i] + record for i, record in enumerate(data_without_id)]
+        self.counter += len(data)
+        cal_data = pd.DataFrame(data_with_id, columns=['ID','Action Performed', 'input1', 'input2'])
+        cal_data.to_csv(self.path, index=False)
+    
+    def fetch_list(self):
+        existing_data = pd.read_csv(self.path)
+        return existing_data.values.tolist()
+    
+    def fetch_data_frame(self):
+        return pd.read_csv(self.path)
+    
+    def clear(self):
+        cal_data = pd.DataFrame([], columns=['ID','Action Performed', 'input1', 'input2'])
+        cal_data.to_csv(self.path, index=False)
